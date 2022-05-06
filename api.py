3b284d13c1d37ed778027bd2c0885115c40e7d5a
Yuxiang Chen <chenyx.eric2013@gmail.com>
2022-05-06 14:40:38 -0400

    added code for the api backend

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 main = Blueprint('main', __name__)
+from engine import RecommendationEngine
 
 import json
 
@@ -15,3 +16,25 @@ def recommendations(user_id, count):
 def get_user_movie_rating(user_id, movie_id):
     user_movie_rating = recommendation_engine.get_user_movie_rating()
     return json.dumps(user_movie_rating)
+
+@main.route("/<int:user_id>/ratings", methods = ["POST"])
+def add_ratings(user_id):
+    # get the ratings from the Flask POST request object
+    ratings_list = request.form.keys()[0].strip().split("\n")
+    ratings_list = map(lambda x: x.split(","), ratings_list)
+    # create a list with the format required by the negine (user_id, movie_id, rating)
+    ratings = map(lambda x: (user_id, int(x[0]), float(x[1])), ratings_list)
+    # add them to the model using then engine API
+    recommendation_engine.add_ratings(ratings)
+ 
+    return json.dumps(ratings)
+ 
+ 
+def create_app(spark_context, dataset_path):
+    global recommendation_engine 
+
+    recommendation_engine = RecommendationEngine(spark_context, dataset_path)    
+    
+    app = Flask(__name__)
+    app.register_blueprint(main)
+    return app 
